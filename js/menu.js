4b72b8ff89daceef1b604f5710fd74995ab8a9ec
jatoni <jat_antonio_torres.juan@outlook.com>
2022-07-12 00:46:35 -0500

    Animacion del menu queda listo y header y menu ya estan

---
+++ js/menu.js
@@ -0,0 +1,17 @@
+var lastScrollTop = 0;
+$(document).ready(function(){
+    $(window).on('scroll',function(){
+        var st = $(this).scrollTop();
+        if(st > lastScrollTop){
+            $('.menu').addClass('menu-fixed');
+            $('.menu').removeClass('menu-Up');
+        }else if(st < lastScrollTop){
+            $('.menu').addClass('menu-Up');
+            $('.menu').removeClass('menu-fixed');
+        }else if(st === 0){
+            $('.menu').removeClass('menu-Up');
+            $('.menu').removeClass('menu-fixed');
+        }
+        lastScrollTop = st;
+    });
+});
